c2f6d34a989206dd22fa0fcbbcbd2235977acf17
とーふとふ <tohu.soy@gmail.com>
2019-05-03 23:29:55 +0900

    24

---
+++ 24/Anmanman.hs
@@ -0,0 +1,8 @@
+module Anmanman (Friend, ai, yuuki) where
+
+data Friend = Friend String deriving Show
+
+ai = Friend "Ai"
+yuuki = Friend "Yuuki"
+
+
---
+++ 24/Some.hs
@@ -0,0 +1,6 @@
+module Some where
+
+a, b, c :: Integer
+a = 3
+b = 4 * c
+c = 5
